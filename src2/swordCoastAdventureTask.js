e1f60a13c859da9fa25f9748139dc0b9f18e4ef1
Quinn Gil <qgil@QGIL-MAC2.local>
2014-03-29 19:36:12 -0700

    SCA wip [this is faster than C&P into js-console]

--- src2/swordCoastAdventureTask.js
+++ src2/swordCoastAdventureTask.js
@@ -130,10 +130,8 @@
         else if (data.state.isModal){
             console.log("isModal");
             task.then(this.clear_modal.bind(this));
-            var new_task = $.nwg.adventure.create(this.character).create_base_task();
-            new_task.start_in(2000);
-            task.finish();
-            return;
+            task.then($.nwg.adventure.create(this.character).check_adventure_state());
+            return {error:false, delay: 3000};
         }
         //else
 
